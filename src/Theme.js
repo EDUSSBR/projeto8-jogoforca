de69d945f67f7b30bfbed86e558d7fd004be99ea
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-02-15 09:50:47 -0300

    Add theme with main colors and font-configs that is different from globals ones

---
+++ src/Theme.js
@@ -0,0 +1,41 @@
+import { ThemeProvider } from "styled-components";
+
+const styles = {
+    colors: {
+
+        lettersBtnBorder: '#7AA7C7',
+        lettersBtnBgDisabled: '#9FAAB5',
+        lettersBtnColorDisabled: '#798A9F',
+        lettersBtnColorEnabled: '#39739D',
+        lettersBtnBgEnabled: '#E1ECF4',
+
+        chooseWordBtnBg: '#27AE60',
+        black: 'black',
+        quarenteneColorRed: '#FF00001',
+        quarenteneColorGreen: '#27AE60',
+
+        knowTheWordBorder: '#CCCCCC',
+        tryWordColor: '#3C76A1',
+        tryWordBorder: '#7AA7C7'
+    },
+    fonts: {
+        choosenWord: {
+            fontSize: '20px',
+            lineHeight: '23px'
+        },
+        quarentine: {
+            fontFamily: ['Noto Sans', 'sans-serif'],
+            fontSize: '50px',
+            lineHeight: '68px'
+        },
+        tryWordLabel: {
+            fontWeight: 400,
+            fontSize: '20px',
+            lineHeight: '23px',
+        },
+    }
+
+}
+export default function Theme({children}){
+    return <ThemeProvider theme={styles}>{children}</ThemeProvider>
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {GlobalResetMeyer} from './Reset'
 import { GlobalStyles } from './Global';
+import Theme  from './Theme';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Theme>
     <GlobalResetMeyer />
     <GlobalStyles />
     <App />
+    </Theme>
   </React.StrictMode>
 );
