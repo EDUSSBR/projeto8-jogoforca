2de23d081f452d0aee4da1c482de3f326f422fba
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-02-15 12:16:40 -0300

    create styled p for rendering the word/underlines

---
+++ src/components/Jogo/RandomWord.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const RandomWord = styled.p`
+    font-family: ${({theme})=> theme.fonts.quarentine.fontFamily};
+    font-size: ${({theme})=> theme.fonts.quarentine.fontSize};
+    line-height: ${({theme})=> theme.fonts.quarentine.lineHeight};
+    margin-right:61px;
+`
