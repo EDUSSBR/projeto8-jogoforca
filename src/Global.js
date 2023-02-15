d21b72d9a230efc4f5aa673878bd7168ef24cbe2
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-02-15 09:16:04 -0300

    add Main font config to Global Styles

--- src/Global.js
+++ src/Global.js
@@ -3,6 +3,11 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
 * {
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    font-size: '16px';
+    line-height: '19px';
+    text-align: 'center';
 }
 
 a {
